3d6a92041f4f14344580842a9db22746847f61c8
Jeroen Egelmeers <mail@jeroenegelmeers.nl>
2015-12-30 14:04:12 +0100

    Bugs fixed so it is runnable.

---
+++ SA-Week3-NarrowCasting/ViewModel/Start.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace SA_Week3_NarrowCasting.ViewModel
+{ 
+    public class Start
+    {
+        private Model.ConcreteSubject data;
+        private List<ConcreteObserver> activeConcreteObserver;
+
+        public Start()
+        {
+            // init variables
+            data = new Model.ConcreteSubject();
+            activeConcreteObserver = new List<ConcreteObserver> {};
+
+            // start with 1 observer
+            createConcreteObserver();
+        }
+
+        public void createConcreteObserver()
+        {
+            activeConcreteObserver.Insert(activeConcreteObserver.Count, new ConcreteObserver(this, data));             
+        }
+
+        public void destroyConcreteObserver(ConcreteObserver o)
+        {
+            activeConcreteObserver.Remove(o);
+        }
+    }
+}
